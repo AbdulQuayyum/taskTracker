ae9419c8e0ff0ec265e902e56ec8dd5129179d79
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-04 13:14:44 +0100

    first commit

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <HeaderItem>
+        <h1>Task Tracker</h1>
+    </HeaderItem>
+</template>
+
+<script>
+export default {
+    name: 'HeaderItem',
+}
+</script>
+
+<style scoped>
+   HeaderItem {
+       display: flex;
+       justify-content: space-between;
+       align-items: center;
+       margin-bottom: 20px;
+   }
+</style>
